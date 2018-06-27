15b1f80156589f9ed86f2aaf2830de6b309f39e1
Matthew Dodi <matthew.dodi@gmail.com>
2018-06-27 13:47:45 -0700

    Added Google Street API and NYTimes Articles API

--- js/script.js
+++ js/script.js
@@ -12,6 +12,26 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var $street = $('#street').val();
+    var $city = $('#city').val();
+    var $streetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=640x500&location= "+$street+", "+$city;
+    console.log($streetViewURL);
+
+    $body.append('<img class="bgimg" src="' +$streetViewURL + '" >');
+
+    //NYTimes API
+    var $newYTAPI = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $city + "&sort=newest&api-key=9c8b4733d797436ea08fb3777c4653af";
+
+    $.getJSON( $newYTAPI, function( data ) {
+        var article = data.response.docs;
+
+        $nytHeaderElem.text('New York Times Articles for ' + $street + ", " + $city);
+        for (var i = 0; i < article.length; i++) {
+            $nytElem.append('<li>' + '<h2><a href="' + article[i].web_url + '" target="_blank" >' + article[i].headline.main + '</a>' + '</h2>' + '<br>' + article[i].snippet + '</li>');
+        }
+
+
+    })
 
     // YOUR CODE GOES HERE!
 
