4fa7206ef5268e5cbbbb84aeaa2b7d373d7edb95
Matthew Dodi <matthew.dodi@gmail.com>
2018-06-28 13:22:16 -0700

    Added style and used the Wikipedia API to show relevant Wikipedia Links

--- js/script.js
+++ js/script.js
@@ -12,24 +12,44 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-    var $street = $('#street').val();
     var $city = $('#city').val();
-    var $streetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=640x500&location= "+$street+", "+$city;
+    var $streetViewURL = "http://maps.googleapis.com/maps/api/streetview?size=640x500&location= "+$city;
     console.log($streetViewURL);
 
-    $body.append('<img class="bgimg" src="' +$streetViewURL + '" >');
+
+    $('.imgContainer').html('<img class="bgimg" src="' +$streetViewURL + '" >');
+
 
     //NYTimes API
     var $newYTAPI = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $city + "&sort=newest&api-key=9c8b4733d797436ea08fb3777c4653af";
-
+    $nytHeaderElem.text('New York Times Articles for ' + $city);
     $.getJSON( $newYTAPI, function( data ) {
-        var article = data.response.docs;
 
-        $nytHeaderElem.text('New York Times Articles for ' + $street + ", " + $city);
+        var article = data.response.docs;
         for (var i = 0; i < article.length; i++) {
             $nytElem.append('<li>' + '<h2><a href="' + article[i].web_url + '" target="_blank" >' + article[i].headline.main + '</a>' + '</h2>' + '<br>' + article[i].snippet + '</li>');
         }
 
+
+    }).error(function(e) {
+        $nytHeaderElem.text('New YorK Times Articles Could Not Be Loaded');
+    })
+
+    var $wikiArticlesAPI = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $city + '&limit=10&namespace=0&format=json';
+    console.log($wikiArticlesAPI);
+
+    $.ajax({
+        url: $wikiArticlesAPI,
+        dataType: "jsonp",
+        success: function( response ) {
+            var wikiArticle = response
+            console.log(wikiArticle);
+
+            for (var i = 1; i < wikiArticle[2].length; i++) {
+                $wikiElem.append('<a href="' + wikiArticle[3][i] + '"> <li>' + wikiArticle[2][i] + '</li> </a>');
+
+            }
+        }
 
     })
 
